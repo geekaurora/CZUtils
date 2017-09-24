c7662f599493fedbbd44919779788b617e451342
Cheng Zhang <czhang@groupon.com>
2017-09-23 17:56:22 -0700

    Unit project

---
+++ CZUtils/CZUtils.h
@@ -0,0 +1,19 @@
+//
+//  CZUtils.h
+//  CZUtils
+//
+//  Created by Administrator on 23/09/2017.
+//  Copyright © 2017 Cheng Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CZUtils.
+FOUNDATION_EXPORT double CZUtilsVersionNumber;
+
+//! Project version string for CZUtils.
+FOUNDATION_EXPORT const unsigned char CZUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CZUtils/PublicHeader.h>
+
+
