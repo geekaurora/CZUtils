5e35405272ed98084c64c2b5825c31c553ea18d1
Cheng Zhang <showt2me@gmail.com>
2020-02-22 21:43:52 +0900

    minor update

--- Sources/CZUtils/CZAlertManager.swift
+++ Sources/CZUtils/CZAlertManager.swift
@@ -9,19 +9,28 @@ import UIKit
 
 /// Convenient helper class for AlertViewController displaying 
 open class CZAlertManager: NSObject {
-    open class func showAlert(title: String? = nil,
-                         message: String,
-                         actionText1: String = "Ok",
-                         actionHandler1: ((UIAlertAction) -> Void)? = nil,
-                         on viewController: UIViewController? = nil,
-                         completion: (() -> Void)? = nil) {
-        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let action = UIAlertAction(title: actionText1, style: .default, handler: actionHandler1)
-        alertController.addAction(action)
-        guard let presentingViewController = viewController ?? UIApplication.shared.keyWindow?.rootViewController else {
-            assertionFailure("Couldn't find valid presenting ViewController.")
-            return
-        }
-        presentingViewController.topMost.present(alertController, animated: true, completion: completion)
+  
+  open class func showAlert(title: String? = nil,
+                            message: String,
+                            confirmText: String = "Ok",
+                            confirmHandler: ((UIAlertAction) -> Void)? = nil,
+                            cancelText: String? = nil,
+                            cancelHandler: ((UIAlertAction) -> Void)? = nil,
+                            on viewController: UIViewController? = nil,
+                            completion: (() -> Void)? = nil) {
+    let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    let action1 = UIAlertAction(title: confirmText, style: .default, handler: confirmHandler)
+    alertController.addAction(action1)
+    if let cancelText = cancelText {
+      let action2 = UIAlertAction(title: cancelText, style: .cancel, handler: cancelHandler)
+      alertController.addAction(action2)
     }
+    
+    guard let presentingViewController = viewController ?? UIApplication.shared.keyWindow?.rootViewController else {
+      assertionFailure("Couldn't find valid presenting ViewController.")
+      return
+    }
+    presentingViewController.topMost.present(alertController, animated: true, completion: completion)
+  }
+  
 }
