32e6b9db7aebdd561203c7c76e34220b1bd63464
Cheng Zhang <showt2me@gmail.com>
2020-04-14 19:15:45 +0900

    Add ThreadSafeHashTable

---
+++ Sources/CZUtils/ThreadSafeHashTable.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+/// Thead-safe HashTable that only holds weak reference to containing items.
+public class ThreadSafeHashTable<ObjectType> : NSObject where ObjectType : AnyObject {
+  
+  private var items = NSHashTable<ObjectType>.weakObjects()
+  private let mutexLock = SimpleThreadLock()
+  
+  public var count: Int {
+    return mutexLock.execute {
+      items.count
+    }
+  }
+  
+  public var allObjects: [ObjectType] {
+    return mutexLock.execute {
+      return items.allObjects
+    }
+  }
+  
+  public func add(_ item: ObjectType) {
+    mutexLock.execute {
+      items.add(item)
+    }
+  }
+  
+  public func remove(_ item: ObjectType) {
+    mutexLock.execute {
+      items.remove(item)
+    }
+  }
+  
+  public func contains(_ item: ObjectType) -> Bool {
+    return mutexLock.execute {
+      items.contains(item)
+    }
+  }
+}
