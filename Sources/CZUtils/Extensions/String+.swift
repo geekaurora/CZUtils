39fede2644d3011c4b22eeab9c7be7f45e748dc9
Cheng Zhang <showt2me@gmail.com>
2020-04-18 03:05:33 +0900

    Add MD5 to String extension.

--- Sources/CZUtils/Extensions/String+.swift
+++ Sources/CZUtils/Extensions/String+.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import CommonCrypto
 
 public extension String {
   /**
@@ -76,6 +77,21 @@ public extension String {
       .map { String($0) }
   }
 
+  /// MD5 HashValue: Should #import <CommonCrypto/CommonCrypto.h> in your Bridging-Header file
+  var MD5: String {
+      let length = Int(CC_MD5_DIGEST_LENGTH)
+      var digest = [UInt8](repeating: 0, count: length)
+      
+      if let d = self.data(using: String.Encoding.utf8) {
+          _ = d.withUnsafeBytes { (body: UnsafePointer<UInt8>) in
+              CC_MD5(body, CC_LONG(d.count), &digest)
+          }
+      }
+      
+      return (0..<length).reduce("") {
+          $0 + String(format: "%02x", digest[$1])
+      }
+  }
 }
 
 public extension NSAttributedString {
