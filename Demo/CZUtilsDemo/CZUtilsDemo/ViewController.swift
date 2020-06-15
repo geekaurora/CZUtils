1de8b8e9cda47940cf5280d6a72885fea93013da
cheng zhang <showt2me@gmail.com>
2020-06-15 20:14:11 +0900

    Add topMostViewController to UIApplication extension.

---
+++ Demo/CZUtilsDemo/CZUtilsDemo/ViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+import CZUtils
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  func test() {
+    
+  }
+}
+
---
+++ Sources/CZUtils/Extensions/UIApplication+.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+public extension UIApplication {
+  /// Returns top most view controller on `inputController`.
+  /// Note: `inputController` defaults to keyWindow.rootViewControlle if input is nil
+  ///
+  /// - Parameter inputController: The  view controller which contains top most controller.
+  /// - Returns: top most view controller on `inputController`.
+  func topMostViewController(on inputController: UIViewController? = nil) -> UIViewController? {
+    let controller = inputController ?? self.keyWindow?.rootViewController
+    
+    if let navigationController = controller as? UINavigationController {
+      return topMostViewController(on: navigationController.visibleViewController)
+    }
+    if let tabController = controller as? UITabBarController {
+      if let selected = tabController.selectedViewController {
+        return topMostViewController(on: selected)
+      }
+    }
+    if let presented = controller?.presentedViewController {
+      return topMostViewController(on: presented)
+    }
+    return controller
+  }
+}
+
+
