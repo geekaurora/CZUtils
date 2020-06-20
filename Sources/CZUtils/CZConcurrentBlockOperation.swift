a00a714508cfe1be47939621ad070a9d4774d4cd
Cheng Zhang <showt2me@gmail.com>
2020-06-20 14:18:21 +0900

    Added CZConcurrentBlockOperation.

---
+++ Sources/CZUtils/CZConcurrentBlockOperation.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+/**
+ When cancel any custom ConcurrentOperation in OperationQueue which maintains its ready/finifhsed/cancelled state,
+ queued operations will disordered.
+ `CZConcurrentBlockOperation` inherits BlockOperation to solve the problem.
+ 
+ ### Usage
+  1. Subclass `CZConcurrentBlockOperation`.
+  2. Override `executeBlock()` with custom execution.
+  3. Invoke `finish()` when concurrent execution is done.
+ */
+@objc open class CZConcurrentBlockOperation: BlockOperation {  
+  private let semaphore = DispatchSemaphore(value: 0)
+  
+  public init(block: @escaping () -> Void) {
+   fatalError("Must call designated intialize init().")
+  }
+    
+  public override init() {
+    super.init()
+    
+    let awaitBlock = {
+      self.executeBlock()
+      self.semaphore.wait()
+    }
+    addExecutionBlock(awaitBlock)
+  }
+  
+  open func executeBlock() {
+    fatalError("\(#function) must be overriden in subclass.")
+  }
+  
+  open func finish() {
+    semaphore.signal()
+  }
+}
