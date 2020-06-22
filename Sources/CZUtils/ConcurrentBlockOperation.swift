e08101f010c7355f39d78f8475188b786ac3d179
Cheng Zhang <showt2me@gmail.com>
2020-06-22 15:59:49 +0900

    Update ConcurrentBlockOperation to better handle cancel().

--- Sources/CZUtils/ConcurrentBlockOperation.swift
+++ Sources/CZUtils/ConcurrentBlockOperation.swift
@@ -10,9 +10,11 @@ import Foundation
  
  - Note: If customize `cancel()` in subclass, should call `super.cancel()` instead of `super.finish()`.
  */
-@objc open class ConcurrentBlockOperation: BlockOperation {
+@objc
+open class ConcurrentBlockOperation: BlockOperation {
   private let semaphore = DispatchSemaphore(value: 0)
   private var _isBlockFinished = false
+  public var props = [String: Any]()
   
   public init(block: @escaping () -> Void) {
     fatalError("Must call designated intialize init().")
@@ -27,7 +29,7 @@ import Foundation
       if (!self._isBlockFinished) {
         self.semaphore.wait()
       }
-      dbgPrint("executeBlock finished.")
+      dbgPrint("executeBlock finished. Class = \(type(of: self)); props = \(self.props)")
     }
     addExecutionBlock(awaitBlock)
   }
@@ -38,6 +40,16 @@ import Foundation
   
   open func finish() {
     _isBlockFinished = true
+    signalFinished()
+  }
+  
+  open override func cancel() {
+    super.cancel()
+    signalFinished()
+    dbgPrint("executeBlock cancelled. Class = \(type(of: self)); props = \(props)")
+  }
+  
+  private func signalFinished() {
     semaphore.signal()
-  }  
+  }
 }
