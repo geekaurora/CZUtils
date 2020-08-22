d293c7f4a967f79d40c48a592cc7bca69304039f
Cheng Zhang <showt2me@gmail.com>
2020-08-22 17:29:55 +0900

    Add removeFile().

--- Sources/CZUtils/CZFileHelper.swift
+++ Sources/CZUtils/CZFileHelper.swift
@@ -9,19 +9,32 @@ import Foundation
 
 /// Helper class for file related methods 
 @objc open class CZFileHelper: NSObject {
-   public static func getFileSize(_ filePath: String?) -> Int? {
-        guard let filePath = filePath else {return nil}
-        do {
-            let attrs = try FileManager.default.attributesOfItem(atPath: filePath)
-            let size =  attrs[.size] as? Int
-            return size
-        } catch {
-            dbgPrint("Failed to get file size of \(filePath). Error - \(error.localizedDescription)")
-        }
-        return nil
+  @objc public static var documentDirectory: String {
+    return NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] + "/"
+  }
+  
+  public static func getFileSize(_ filePath: String?) -> Int? {
+    guard let filePath = filePath else { return nil }
+    do {
+      let attrs = try FileManager.default.attributesOfItem(atPath: filePath)
+      let size =  attrs[.size] as? Int
+      return size
+    } catch {
+      dbgPrint("Failed to get file size of \(filePath). Error - \(error.localizedDescription)")
     }
-    
-    @objc public static var documentDirectory: String {
-        return NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] + "/"
+    return nil
+  }
+  
+  /**
+    Remove local file at `url`.       
+   */
+  public static func removeFile(_ url: URL?) {
+    guard let url = url else { return }
+    do {
+      try FileManager.default.removeItem(at: url)
+    } catch let error as NSError {
+      dbgPrint("Failed to remove file - \(url). Error - \(error.localizedDescription)")
     }
+    return
+  }
 }
