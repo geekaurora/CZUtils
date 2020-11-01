34552aaf2fb324112d925750cd67dc170e372e0e
Cheng Zhang <showt2me@gmail.com>
2020-11-01 18:49:56 +0900

    Update UIButton extension.

--- Sources/CZUtils/Extensions/UIButton+.swift
+++ Sources/CZUtils/Extensions/UIButton+.swift
@@ -10,44 +10,51 @@ import UIKit
 
 private var controlHandlerKey: Int8 = 0
 public extension UIButton {
-    /**
-      Add self-contained action handler for button
-     */
+  /**
+   Add self-contained action handler for button
+   */
   func addHandler(for controlEvents: UIControl.Event, handler: @escaping (UIButton) -> ()) {
-        if let oldTarget = objc_getAssociatedObject(self, &controlHandlerKey) as? CocoaTarget<UIButton> {
-            self.removeTarget(oldTarget, action: #selector(oldTarget.sendNext), for: controlEvents)
-        }
-        
-        let target = CocoaTarget<UIButton>(handler)
-        objc_setAssociatedObject(self, &controlHandlerKey, target, .OBJC_ASSOCIATION_RETAIN)
-        self.addTarget(target, action: #selector(target.sendNext), for: controlEvents)
+    if let oldTarget = objc_getAssociatedObject(self, &controlHandlerKey) as? CocoaTarget<UIButton> {
+      self.removeTarget(oldTarget, action: #selector(oldTarget.sendNext), for: controlEvents)
     }
     
-    /**
-      Set image with tintColor for desired controlState
-     */
+    let target = CocoaTarget<UIButton>(handler)
+    objc_setAssociatedObject(self, &controlHandlerKey, target, .OBJC_ASSOCIATION_RETAIN)
+    self.addTarget(target, action: #selector(target.sendNext), for: controlEvents)
+  }
+  
+  /**
+   Set image with tintColor for desired controlState
+   */
   func setImage(_ imageName: String, for controlState: UIControl.State = .normal, tintColor: UIColor) {
-        let image = UIImage(named: imageName)?.withRenderingMode(.alwaysTemplate)
-        setImage(image, for: controlState)
-        self.tintColor = tintColor
-    }
+    setImage(UIImage(named: imageName), for: controlState, tintColor: tintColor)
+  }
+  
+  /**
+   Set image with tintColor for desired controlState
+   */
+  func setImage(_ image: UIImage?, for controlState: UIControl.State = .normal, tintColor: UIColor) {
+    let image = image?.withRenderingMode(.alwaysTemplate)
+    setImage(image, for: controlState)
+    self.tintColor = tintColor
+  }
 }
 
 /**
-  A target that accepts action messages
+ A target that accepts action messages
  */
 public final class CocoaTarget<T>: NSObject {
-    private let action: (T) -> ()
-    
-    public init(_ action: @escaping (T) -> ()) {
-        self.action = action
+  private let action: (T) -> ()
+  
+  public init(_ action: @escaping (T) -> ()) {
+    self.action = action
+  }
+  
+  @objc
+  public func sendNext(_ receiver: Any?) {
+    guard let receiver = receiver as? T else {
+      preconditionFailure("`receiver` isn't expected type.")
     }
-    
-    @objc
-    public func sendNext(_ receiver: Any?) {
-        guard let receiver = receiver as? T else {
-            preconditionFailure("`receiver` isn't expected type.")
-        }
-        action(receiver)
-    }
+    action(receiver)
+  }
 }
