11071796592046ae1f50929484fe77dfa3a224f5
Cheng Zhang <showt2me@gmail.com>
2020-11-05 18:44:12 +0900

    Separate WeakWrapper.

--- Sources/CZUtils/ThreadSafeWeakArray.swift
+++ Sources/CZUtils/ThreadSafeWeakArray.swift
@@ -96,16 +96,3 @@ private class AnyThreadSafeWeakArray<Element: AnyObject> {
     }
   }
 }
-
-/// Wrapper that only holds weak reference to containing element.
-public class WeakWrapper<T: AnyObject> {
-  public private(set) weak var element: T?
-  public init(_ element: T) {
-    self.element = element
-  }
-}
-
-
-
-
-
---
+++ Sources/CZUtils/WeakWrapper.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+/// Wrapper that only holds weak reference to containing element.
+public class WeakWrapper<T: AnyObject> {
+  public private(set) weak var element: T?
+  public init(_ element: T) {
+    self.element = element
+  }
+}
+
+
+
+
+
