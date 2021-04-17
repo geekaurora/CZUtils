b20a888b837832ef09bac7c3bfabe1b6fdce89f6
Cheng Zhang <showt2me@gmail.com>
2021-04-17 23:13:25 +0900

    minor.

--- Sources/CZUtils/UI/FPSLabel/CZFPSLabel.swift
+++ Sources/CZUtils/UI/FPSLabel/CZFPSLabel.swift
@@ -5,15 +5,7 @@ public class CZFPSLabel: UILabel {
     public static let size = CGSize(width: 55, height: 20)
     public static let backgroundColor = UIColor(white: 0.1, alpha: 1)
   }
-  
-  private lazy var displayLinkMonitor: CADisplayLinkMonitor = {
-    let displayLinkMonitor = CADisplayLinkMonitor()
-    displayLinkMonitor.delegate = self
-    return displayLinkMonitor
-  }()
-  private var link: CADisplayLink!
-  private var lastTime: TimeInterval = 0
-  private var frames: Int = 0
+  private var displayLinkMonitor: CADisplayLinkMonitor!
   
   public override init(frame theFrame: CGRect) {
     var frame = theFrame
@@ -23,35 +15,12 @@ public class CZFPSLabel: UILabel {
     super.init(frame: frame)
     setupViews()
     
-//    self.link = CADisplayLink(target: self, selector: #selector(tick(_:)))
-//    link.add(to: .main, forMode: .common)
+    // Initialize CADisplayLinkMonitor.
+    displayLinkMonitor = CADisplayLinkMonitor()
+    displayLinkMonitor.delegate = self
   }
 
   public required init?(coder: NSCoder) { fatalError() }
-  
-  deinit {
-    link.invalidate()
-  }
-  
-  // MARK: - CADisplayLink
-  
-  @objc func tick(_ link: CADisplayLink) {
-    guard lastTime != 0 else {
-      lastTime = link.timestamp
-      return
-    }
-    
-    frames += 1
-    let timeDelta = link.timestamp - lastTime
-    if timeDelta < 1 {
-      return
-    }
-    
-    lastTime = link.timestamp
-    let fps = Double(frames) / Double(timeDelta)
-    frames = 0
-    updateText(fps: fps)
-  }
   
   // MARK: - Display
   
