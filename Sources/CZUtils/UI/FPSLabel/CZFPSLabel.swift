79aaa069a6c5314f068db97e9018ff27377f3595
Cheng Zhang <showt2me@gmail.com>
2021-04-17 22:50:02 +0900

    Minor update.

--- Sources/CZUtils/UI/FPSLabel/CZFPSLabel.swift
+++ Sources/CZUtils/UI/FPSLabel/CZFPSLabel.swift
@@ -1,8 +1,9 @@
 import UIKit
 
 public class CZFPSLabel: UILabel {
-  private enum Constant {
-    static let size = CGSize(width: 55, height: 20)
+  public enum Constant {
+    public static let size = CGSize(width: 55, height: 20)
+    public static let backgroundColor = UIColor(white: 0.1, alpha: 1)
   }
   private var link: CADisplayLink!
   private var lastTime: TimeInterval = 0
@@ -60,6 +61,10 @@ public class CZFPSLabel: UILabel {
       self.alignBottom(to: view.safeAreaLayoutGuide, constant: 5)
     ])
   }
+  
+  public override func sizeThatFits(_ size: CGSize) -> CGSize {
+    Constant.size
+  }
 }
 
 // MARK: - Private methods
@@ -67,13 +72,17 @@ public class CZFPSLabel: UILabel {
 private extension CZFPSLabel {
   func setupViews() {
     font = UIFont(name: "Menlo", size: 14)
-    backgroundColor = UIColor(white: 0.9, alpha: 1)
-    textColor = .black
+    backgroundColor = Constant.backgroundColor
+    textColor = UIColor(red: 200.0 / 255.0, green: 0, blue: 0, alpha: 1)
     roundCorner(3)
   }
   
   func updateText(fps: Double) {
     let fpsInt = Int(round(fps))
     text = "\(fpsInt) FPS"
+    
+    let progress = fps / 60.0;
+    let color = UIColor(hue: CGFloat(0.27 * (progress - 0.2)), saturation: 1, brightness: 1, alpha: 1)
+    self.textColor = color
   }
 }
