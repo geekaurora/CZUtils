92383669e73ce0b38317d669d389eb90f127fb82
Cheng Zhang <showt2me@gmail.com>
2021-04-17 19:26:16 +0900

    built

--- Sources/CZUtils/UI/FPSLabel/CZFPSLabel.swift
+++ Sources/CZUtils/UI/FPSLabel/CZFPSLabel.swift
@@ -7,15 +7,15 @@ public class CZFPSLabel: UILabel {
   
   private var link: CADisplayLink!
   
-  private var _lastTime: TimeInterval = 0
-  private var _count: Int = 0
+  private var lastTime: TimeInterval = 0
+  private var count: Int = 0
   private var _llll: TimeInterval = 0
   private var subFont: UIFont?
   
   public override init(frame: CGRect) {
     super.init(frame: frame)
     font = UIFont(name: "Menlo", size: 14)
-
+    
     self.link = CADisplayLink(target: self, selector: #selector(tick(_:)))
     link.add(to: .main, forMode: .common)
     backgroundColor = .red
@@ -32,34 +32,31 @@ public class CZFPSLabel: UILabel {
   }
   
   @objc func tick(_ link: CADisplayLink) {
-    // dbgPrintWithFunc(self, "link = \(link)")
-    
-    if (_lastTime == 0) {
-      _lastTime = link.timestamp;
-        return;
+    guard lastTime != 0 else {
+      lastTime = link.timestamp
+      return
     }
     
-    _count += 1;
-    var delta = link.timestamp - _lastTime;
-    if (delta < 1) {
-      return;
+    count += 1
+    let delta = link.timestamp - lastTime
+    if delta < 1 {
+      return
     }
-    _lastTime = link.timestamp;
-    let fps = Double(_count) / Double(delta);
-    _count = 0;
+    lastTime = link.timestamp
+    let fps = Double(count) / Double(delta)
+    count = 0
     
-    let progress = fps / 60.0;
+    let progress = fps / 60.0
     
-//    UIColor *color = [UIColor colorWithHue:0.27 * (progress - 0.2) saturation:1 brightness:0.9 alpha:1];
-//    NSMutableAttributedString *text = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"%d FPS",(int)round(fps)]];
-//    [text yy_setColor:color range:NSMakeRange(0, text.length - 3)];
-//    [text yy_setColor:[UIColor whiteColor] range:NSMakeRange(text.length - 3, 3)];
-//    text.yy_font = _font;
-//    [text yy_setFont:_subFont range:NSMakeRange(text.length - 4, 1)];
-//
-//    self.attributedText = text;
+    //    UIColor *color = [UIColor colorWithHue:0.27 * (progress - 0.2) saturation:1 brightness:0.9 alpha:1]
+    //    NSMutableAttributedString *text = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"%d FPS",(int)round(fps)]]
+    //    [text yy_setColor:color range:NSMakeRange(0, text.length - 3)]
+    //    [text yy_setColor:[UIColor whiteColor] range:NSMakeRange(text.length - 3, 3)]
+    //    text.yy_font = _font
+    //    [text yy_setFont:_subFont range:NSMakeRange(text.length - 4, 1)]
+    //
+    //    self.attributedText = text
     
-
     let fpsInt = Int(round(fps))
     self.text = "\(fpsInt) FPS"
   }
