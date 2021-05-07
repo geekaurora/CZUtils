133e7db830bd528c6ed8227b65fd1bce1e000665
Cheng Zhang <showt2me@gmail.com>
2021-05-07 22:27:06 +0900

    enable test

--- Tests/CZUtilsTests/DataStructureTests/ThreadSafeDictionaryTests.swift
+++ Tests/CZUtilsTests/DataStructureTests/ThreadSafeDictionaryTests.swift
@@ -27,32 +27,32 @@ class ThreadSafeDictionaryTests: XCTestCase {
     
   // TODO: Fix the test.
     func testMultiThreadSetValue() {
-//        // 1. GIVEN(Condition) - some context: Describes the system's tate before any processing
-//        // Initialize ThreadSafeDictionary
-//        let threadSafeDict = ThreadSafeDictionary<Int, Int>()
-//
-//        // Concurrent DispatchQueue to simulate multiple-thread read/write executions
-//        let queue = DispatchQueue(label: Self.queueLabel,
-//                                  qos: .userInitiated,
-//                                  attributes: .concurrent)
-//
-//        // 2. WHEN(Execution) - some action is carried out: Apply some business logic on the given context
-//        // Group asynchonous write operations by DispatchGroup
-//        let dispatchGroup = DispatchGroup()
-//        for (key, value) in originalDict {
-//            dispatchGroup.enter()
-//            queue.async {
-//                // Sleep to simulate operation delay in multiple thread mode
-//                let sleepInternal = TimeInterval((arc4random() % 10)) * 0.00001
-//                Thread.sleep(forTimeInterval: sleepInternal)
-//                threadSafeDict[key] = value
-//                dispatchGroup.leave()
-//            }
-//        }
-//
-//        // 3. THEN(Assertion) - a particular set of observable consequences should be obtained
-//        // DispatchGroup: Wait for completion signal of all operations
-//        dispatchGroup.wait()
-//        XCTAssert(threadSafeDict.isEqual(toDictionary: self.originalDict), "Result of ThreadSafeDictionary should same as the original dictionary.")
+        // 1. GIVEN(Condition) - some context: Describes the system's tate before any processing
+        // Initialize ThreadSafeDictionary
+        let threadSafeDict = ThreadSafeDictionary<Int, Int>()
+
+        // Concurrent DispatchQueue to simulate multiple-thread read/write executions
+        let queue = DispatchQueue(label: Self.queueLabel,
+                                  qos: .userInitiated,
+                                  attributes: .concurrent)
+
+        // 2. WHEN(Execution) - some action is carried out: Apply some business logic on the given context
+        // Group asynchonous write operations by DispatchGroup
+        let dispatchGroup = DispatchGroup()
+        for (key, value) in originalDict {
+            dispatchGroup.enter()
+            queue.async {
+                // Sleep to simulate operation delay in multiple thread mode
+                let sleepInternal = TimeInterval((arc4random() % 10)) * 0.00001
+                Thread.sleep(forTimeInterval: sleepInternal)
+                threadSafeDict[key] = value
+                dispatchGroup.leave()
+            }
+        }
+
+        // 3. THEN(Assertion) - a particular set of observable consequences should be obtained
+        // DispatchGroup: Wait for completion signal of all operations
+        dispatchGroup.wait()
+        XCTAssert(threadSafeDict.isEqual(toDictionary: self.originalDict), "Result of ThreadSafeDictionary should same as the original dictionary.")
     }
 }
