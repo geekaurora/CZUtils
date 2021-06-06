c11f8df3ae0172ed0064ac5afbd790817b5f73e7
Cheng Zhang <showt2me@gmail.com>
2021-06-06 13:26:59 +0900

    Update WeakWrapperTests.

--- Sources/CZUtils/DataStructure/WeakWrapper.swift
+++ Sources/CZUtils/DataStructure/WeakWrapper.swift
@@ -3,9 +3,9 @@ import Foundation
 /// Wrapper that only holds weak reference to containing element.
 public class WeakWrapper<T: AnyObject> {
   
-  public private(set) weak var element: T?
+  weak var element: T?
   
   public init(_ element: T) {
     self.element = element
-  }  
+  }
 }
--- Tests/CZUtilsTests/DataStructureTests/WeakWrapperTests.swift
+++ Tests/CZUtilsTests/DataStructureTests/WeakWrapperTests.swift
@@ -2,8 +2,25 @@ import XCTest
 @testable import CZUtils
 
 class WeakWrapperTests: XCTestCase {
+  private var weakArray: ThreadSafeWeakArray<TestClass>!
+  
+  override func setUp() {
+    weakArray = ThreadSafeWeakArray<TestClass>()
+  }
   
   func testWeakReference() {
+    // Append `object` to `weakArray`.
+    var object: TestClass? = TestClass()
+    weakArray.append(object!)
+    XCTAssert(weakArray.contains(object!))
+    XCTAssertEqual(weakArray.count, 1)
+    
+    // Release `object`: expect `object` in `weakArray` is also released.
+    object = nil
+    XCTAssertEqual(weakArray.count, 0)
+  }
+  
+  func testWeakReference2() {
     var object: TestClass? = TestClass()
     let weakWrapper = WeakWrapper(object!)
     
@@ -12,6 +29,7 @@ class WeakWrapperTests: XCTestCase {
   
     // Release `object`: expect `object` is also released.
     object = nil
+    weakWrapper.element = nil
     XCTAssertTrue(weakWrapper.element === nil)
   }
 }
