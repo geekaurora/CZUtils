c0b94b6255510d12130406f1c087727ba1aea279
Cheng Zhang <showt2me@gmail.com>
2021-06-30 15:38:58 +0900

    [MileStone] Nested locks: should be the same order - avoid the dead lock.

--- Tests/CZUtilsTests/Core/ThreadSafePropertyWrapperTests.swift
+++ Tests/CZUtilsTests/Core/ThreadSafePropertyWrapperTests.swift
@@ -3,15 +3,12 @@ import XCTest
 
 /// Tests of @ThreadSafe property wrapper.
 class ThreadSafePropertyWrapperTests: XCTestCase {
-   static let total = 30000
-//   static let total = 200
+  static let total = 30000
   static let queueLable = "com.czutils.tests"
   
   let lock = NSLock()
   var countWithLock: Int = 0
-  
   @ThreadSafe var count: Int = 0
-  @ThreadSafe var count2: Int = 0
   
   override func setUp() {
     count = 0
@@ -19,7 +16,7 @@ class ThreadSafePropertyWrapperTests: XCTestCase {
   }
   
   // MARK: - Read
-    
+  
   func testReadMultiThread() {
     let dispatchGroup = DispatchGroup()
     
@@ -31,28 +28,15 @@ class ThreadSafePropertyWrapperTests: XCTestCase {
         self.increaseCount()
         
         // Verify read result of `count`.
-//        self.lock.lock()
-//        let countWithLock = self.countWithLock
-//        self.lock.unlock()
-//        XCTAssertEqual(self.count, countWithLock)
-        
-        /** Passed. */
-//        self._count.threadLock { (_count) -> Void in
-//          self.lock.lock()
-//          XCTAssertEqual(_count, self.countWithLock)
-//          self.lock.unlock()
-        
-        /** Passed. */
-        self._count.threadLock { (_count) -> Void in
-          self._count2.threadLock { (_count2) -> Void in
-            XCTAssertEqual(_count, _count2)
-          }
-        }
-        
-        // Different lock session.
-        //XCTAssertEqual(self.count, self.count2)
-        
+        // Nested locks - should be the same order to avoid the dead lock.
+        self.lock.lock()
+        let countWithLock = self.countWithLock
+        XCTAssertEqual(self.count, countWithLock)
+        self.lock.unlock()
         dispatchGroup.leave()
+        
+        // Not work - different lock sessions.
+        // XCTAssertEqual(self.count, self.count)
       }
     }
     // Wait till group multi thread tasks complete.
@@ -82,47 +66,35 @@ class ThreadSafePropertyWrapperTests: XCTestCase {
   }
   
   /// Directly assign isn't thread safe.
-//  func testDirectlyAssignMultiThread() {
-//    let dispatchGroup = DispatchGroup()
-//
-//    // Test increment `count` on multiple threads.
-//    let queue = DispatchQueue(label: Self.queueLable, attributes: .concurrent)
-//    (0..<Self.total).forEach { _ in
-//      dispatchGroup.enter()
-//      queue.async {
-//        self.count += 1
-//        dispatchGroup.leave()
-//      }
-//    }
-//    // Wait till group multi thread tasks complete.
-//    dispatchGroup.wait()
-//    // Verify `count` isn't `Self.total`.
-//    XCTAssertTrue(count != Self.total)
-//  }
-  
-  private func increaseCount() {
-    // sleep(UInt32.random(in: 0..<5) * UInt32(0.001))
-    
-    self._count.threadLock { (_count) -> Void in
-      self._count2.threadLock { (_count2) -> Void in
-        _count += 1
-        _count2 = _count
-      }
-    }
-    
-    
-//    _count.threadLock { (_count) -> Void in
-////      lock.lock()
-//      _count += 1
-////      self.countWithLock = _count
-////      lock.unlock()
-//
-//    }
-//
-//    _count2.threadLock { (_count2) -> Void in
-//      _count2 = self.count
-//    }
-    
-  }
+  //  func testDirectlyAssignMultiThread() {
+  //    let dispatchGroup = DispatchGroup()
+  //
+  //    // Test increment `count` on multiple threads.
+  //    let queue = DispatchQueue(label: Self.queueLable, attributes: .concurrent)
+  //    (0..<Self.total).forEach { _ in
+  //      dispatchGroup.enter()
+  //      queue.async {
+  //        self.count += 1
+  //        dispatchGroup.leave()
+  //      }
+  //    }
+  //    // Wait till group multi thread tasks complete.
+  //    dispatchGroup.wait()
+  //    // Verify `count` isn't `Self.total`.
+  //    XCTAssertTrue(count != Self.total)
+  //  }
 }
 
+// MARK: - Private methods
+
+private extension ThreadSafePropertyWrapperTests {
+  func increaseCount() {
+    // Nested locks - should be the same order to avoid the dead lock.
+    lock.lock()
+    _count.threadLock { (_count) -> Void in
+      _count += 1
+      self.countWithLock = _count
+    }
+    lock.unlock()
+  }
+}
