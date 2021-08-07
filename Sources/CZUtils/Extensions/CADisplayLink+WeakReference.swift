90767856ffc9b0336d0fe9dec1f6e98fdd0d1500
Cheng Zhang <showt2me@gmail.com>
2021-08-07 20:49:06 +0900

    CADisplayLinkWeakReferenceBox

--- Sources/CZUtils/Extensions/CADisplayLink+WeakReference.swift
+++ Sources/CZUtils/Extensions/CADisplayLink+WeakReference.swift
@@ -1,23 +1,37 @@
 import UIKit
 
-class CADisplayLinkWeakReferenceBox {
-  weak var target: AnyObject?
-  
-  init(target: AnyObject, selector sel: Selector) {
-    self.target = target
-  }
-  
-  @objc func tick(_ displayLink: CADisplayLink) {
-    
-  }
-  
-}
-
+/**
+ Exposes the helper method to create the displayLink being held weak reference.
+ */
 public extension CADisplayLink {
+  /// Creates displayLink being held weak reference.
+  /// - Parameters:
+  ///   - target: The target that observes the displayLink, `target` is being held weak reference.
+  ///   - selector: The selector to be called when the screen gets updated.
+  /// - Returns: The displayLink being created.
   class func displayLinkWithWeakTarget(target: AnyObject,
                                        selector: Selector) -> CADisplayLink {
     let weakReferenceBox = CADisplayLinkWeakReferenceBox(target: target, selector: selector)
     return CADisplayLink(target: weakReferenceBox, selector: #selector(CADisplayLinkWeakReferenceBox.tick(_:)))
-    
+  }
+}
+
+// MARK: - Helper Class
+
+private class CADisplayLinkWeakReferenceBox {
+  private weak var target: AnyObject?
+  private let selector: Selector
+  
+  init(target: AnyObject, selector: Selector) {
+    self.target = target
+    self.selector = selector
+  }
+  
+  @objc func tick(_ displayLink: CADisplayLink) {
+    if let target = target {
+      let _ = target.perform(selector)
+    } else {
+      displayLink.invalidate()
+    }
   }
 }
