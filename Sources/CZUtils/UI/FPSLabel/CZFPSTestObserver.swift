48f6da36eec36311bf85d22a8162eb92980d7e24
Cheng Zhang <showt2me@gmail.com>
2021-08-23 23:18:25 +0900

    minor

--- Sources/CZUtils/UI/FPSLabel/CZFPSTestObserver.swift
+++ Sources/CZUtils/UI/FPSLabel/CZFPSTestObserver.swift
@@ -4,5 +4,29 @@ import Foundation
  CADisplayLink observer that observes FPS for FPS tests.
  */
 public class CZFPSTestObserver {
+  public static let shared = CZFPSTestObserver()
   
+  public private(set) var fpsValues = [Double]()
+  
+  public init() {}
+  
+  public func start() {
+    CADisplayLinkMonitor.shared.addListener(self)
+  }
+  
+  public func stop() {
+    CADisplayLinkMonitor.shared.removeListener(self)
+  }
+}
+
+// MARK: - CADisplayLinkObserverProtocol
+
+extension CZFPSTestObserver: CADisplayLinkObserverProtocol {
+  public func handleUpdatedData(_ data: CZEventData?) {
+    guard let displayLinkEventData = (data as? DisplayLinkEventData).assertIfNil,
+          let fps = displayLinkEventData.fps.assertIfNil else {
+      return
+    }
+    fpsValues.append(fps)
+  }
 }
