084a4cc0e08c5d6799b8f897865e695b5d32ea36
Cheng Zhang <showt2me@gmail.com>
2021-10-11 15:54:21 +0900

    demo

--- Samples/CZUtilsDemo/CZUtilsDemo/MainViewController.swift
+++ Samples/CZUtilsDemo/CZUtilsDemo/MainViewController.swift
@@ -2,39 +2,8 @@ import UIKit
 import CZUtils
 
 class MainViewController: UIViewController {
-  var timer: Timer?
-  var weakTimer: CZWeakTimer?
-  
-//  let timer = CZDispatchSourceTimer(timeInterval: 1) {
-//    print("CZDispatchSourceTimer - ticking .. Thread.current = \(Thread.current)")
-//  }
-  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // timer.start()
-    
-//    Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
-//      dbgPrint("tick ..")
-//    }
-    
-    weakTimer = CZWeakTimer.scheduledTimer(
-      timeInterval: 1,
-      target: self,
-      selector: #selector(tick(_:)),
-      userInfo: nil,
-      repeats: true)
-    
-//    timer = Timer.scheduledTimer(
-//      timeInterval: 1,
-//      target: self,
-//      selector: #selector(tick(_:)),
-//      userInfo: nil,
-//      repeats: true)
-  }
-  
-  @objc
-  func tick(_ timer: Timer) {
-    dbgPrint("tick ..")
   }
   
   override func viewDidAppear(_ animated: Bool) {
