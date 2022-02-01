79b0188e00b90551041c1280c6b51fadf1b715e3
Cheng Zhang <756876+geekaurora@users.noreply.github.com>
2022-02-01 22:35:17 +0900

    Bump target OS version to iOS 12 and add SignpostHelper. (#34)
    * minor
    
    * minor
    
    * Add dynamic support for `eventName` - Xcode compiler requires eventName to be static string.
    
    * minor

---
+++ Sources/CZUtils/TestUtils/CZSignpostHelper.swift
@@ -0,0 +1,59 @@
+import os.signpost
+
+/**
+ Helper of os_signpost() method.
+ 
+ - Note: Should select signpost profiling template when starting instrument.
+ 
+ ### Usage
+ ```
+ CZSignpostHelper.shared.start()
+ CZSignpostHelper.shared.end()
+ ```
+ */
+public class CZSignpostHelper {
+  
+  public static let shared = CZSignpostHelper()
+  
+  public enum Constant {
+    public static let subsystem = "com.cz.app"
+    public static let category = "CZSignpostEvents"
+    public static let eventName: StaticString = "CZLoadFileEvent"
+    public static let subeventName: StaticString = "CZLoadFileEvent"
+  }
+  
+  private let log: OSLog
+  private let signpostID: OSSignpostID
+  
+  public init() {
+    log = OSLog(subsystem: Constant.subsystem, category: Constant.category)
+    signpostID = OSSignpostID(log: log)
+  }
+    
+  /// Starts the event with the params.
+  ///
+  /// - Note: `eventName` / `subeventName` should be `StaticString` - immutable and decided during compile time.
+  public func start(eventName: StaticString = Constant.eventName,
+                    subeventName: StaticString = Constant.subeventName) {
+    os_signpost(
+      .begin,
+      log: log,
+      name: eventName,
+      signpostID: signpostID,
+      subeventName)
+  }
+  
+  /// Ends the event with the params.
+  ///
+  /// - Note: `eventName` / `subeventName` should be `StaticString` - immutable and decided during compile time.
+  public func end(eventName: StaticString = Constant.eventName,
+                  subeventName: StaticString = Constant.subeventName)  {
+    os_signpost(
+      .end,
+      log: log,
+      name: eventName,
+      signpostID: signpostID,
+      subeventName)
+  }
+  
+}
