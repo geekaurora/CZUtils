be2a8bc560b786afcce468c3f5c6dcf7b4c08c46
cheng zhang <showt2me@gmail.com>
2022-02-16 19:39:02 +0900

    built

--- Samples/CZUtilsDemo/CZUtilsDemo/MainViewController.swift
+++ Samples/CZUtilsDemo/CZUtilsDemo/MainViewController.swift
@@ -2,6 +2,42 @@ import UIKit
 import CZUtils
 
 class MainViewController: UIViewController {
+  private static let queue = DispatchQueue(
+      label: "LensDiscoveryAssetsProvider", attributes: [])
+
+  @objc public static var animationJSONPath: String? = ""
+
+  private enum AssetStatus {
+    case unknown
+    case available
+    case unavailable
+  }
+
+  private static var assetStatus: AssetStatus = .unknown
+
+  @objc public static func checkAssetAvailability(completion: @escaping (Bool) -> Void) {
+    queue.async {
+      switch assetStatus {
+      case .available:
+        completion(true)
+
+      case .unavailable:
+        completion(false)
+
+      case .unknown:
+        guard let path = animationJSONPath else {
+          completion(false)
+          return
+        }
+        let result = FileManager.default.isReadableFile(atPath: path)
+        queue.sync {
+          assetStatus = result ? .available : .unavailable
+        }
+        completion(result)
+      }
+    }
+  }
+
   override func viewDidLoad() {
     super.viewDidLoad()
   }
