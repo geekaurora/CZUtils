655b842784d6fffe46f569bba5411f392a8bdb9a
Cheng Zhang <showt2me@gmail.com>
2022-04-03 12:19:04 +0900

    Minor update

--- Tests/CZUtilsTests/DataStructureTests/ThreadSafeDictionaryTests.swift
+++ Tests/CZUtilsTests/DataStructureTests/ThreadSafeDictionaryTests.swift
@@ -7,46 +7,47 @@ class ThreadSafeDictionaryTests: XCTestCase {
     static let count = 30000 // 100
   }
   
-  private var originalDict: [Int: Int] = {
-    var originalDict = [Int: Int]()
+  private let expectedDict: [Int: Int] = {
+    var expectedDict = [Int: Int]()
     for (i, value) in (0..<Constant.count).enumerated() {
-      originalDict[i] = value
+      expectedDict[i] = value
     }
-    return originalDict
+    return expectedDict
   }()
   
-  func testSingleThreadInitializ() {
-    let threadSafeDict = ThreadSafeDictionary<Int, Int>(dictionary: originalDict)
-    XCTAssert(threadSafeDict.isEqual(toDictionary: originalDict), "Result of ThreadSafeDictionary should same as the original dictionary.")
+  func testSingleThreadInitialization() {
+    let threadSafeDict = ThreadSafeDictionary<Int, Int>(dictionary: expectedDict)
+    XCTAssert(threadSafeDict.isEqual(toDictionary: expectedDict), "Result of ThreadSafeDictionary should same as the original dictionary.")
   }
   
   func testSingleThreadSetValue() {
     let threadSafeDict = ThreadSafeDictionary<Int, Int>()
-    for (key, value) in originalDict {
+    for (key, value) in expectedDict {
       threadSafeDict[key] = value
     }
-    XCTAssert(threadSafeDict.isEqual(toDictionary: originalDict), "Result of ThreadSafeDictionary should same as the original dictionary.")
+    XCTAssert(threadSafeDict.isEqual(toDictionary: expectedDict), "Result of ThreadSafeDictionary should same as the original dictionary.")
   }
   
-//  func testMultiThreadSetValueManyTimes() {
-//    (0..<300).forEach { _ in
-//      self.testMultiThreadSetValue()
-//    }
-//  }
+  //  func testMultiThreadSetValueManyTimes() {
+  //    (0..<300).forEach { _ in
+  //      self.testMultiThreadSetValue()
+  //    }
+  //  }
   
   func testMultiThreadSetValue() {
     // 1. Initialize ThreadSafeDictionary
     let threadSafeDict = ThreadSafeDictionary<Int, Int>()
     
     // Concurrent DispatchQueue to simulate multiple-thread read/write executions
-    let queue = DispatchQueue(label: Self.queueLabel,
-                              qos: .userInitiated,
-                              attributes: .concurrent)
+    let queue = DispatchQueue(
+      label: Self.queueLabel,
+      qos: .userInitiated,
+      attributes: .concurrent)
     
-    // 2. Copy keys/values from `originalDict` to `threadSafeDict` on multi threads.
+    // 2. Copy keys/values from `expectedDict` to `threadSafeDict` on multi threads.
     // Group asynchonous write operations by DispatchGroup
     let dispatchGroup = DispatchGroup()
-    for (key, value) in originalDict {
+    for (key, value) in expectedDict {
       dispatchGroup.enter()
       queue.async {
         // Sleep to simulate operation delay in multiple thread mode
@@ -59,6 +60,6 @@ class ThreadSafeDictionaryTests: XCTestCase {
     
     // 3. DispatchGroup: Wait for completion signal of all operations
     dispatchGroup.wait()
-    XCTAssert(threadSafeDict.isEqual(toDictionary: self.originalDict), "Result of ThreadSafeDictionary should same as the original dictionary.")
+    XCTAssert(threadSafeDict.isEqual(toDictionary: self.expectedDict), "Result of ThreadSafeDictionary should same as the original dictionary.")
   }
 }
