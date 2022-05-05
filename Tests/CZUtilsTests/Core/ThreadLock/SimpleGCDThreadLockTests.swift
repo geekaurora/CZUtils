7688f303ade72ee8e263d9cc07b4fdcbd4a1edec
Cheng Zhang <showt2me@gmail.com>
2022-05-05 21:54:26 +0900

    minor

--- Tests/CZUtilsTests/Core/ThreadLock/SimpleGCDThreadLockTests.swift
+++ Tests/CZUtilsTests/Core/ThreadLock/SimpleGCDThreadLockTests.swift
@@ -2,7 +2,7 @@ import XCTest
 @testable import CZUtils
 
 class SimpleGCDThreadLockTests: XCTestCase {
-  static let total = 3000
+  static let total = 300
   static let queueLable = "com.czutils.tests"
   
   private var queue: DispatchQueue!
@@ -17,21 +17,21 @@ class SimpleGCDThreadLockTests: XCTestCase {
   func testMultiThread() {
     queue = DispatchQueue(label: Self.queueLable, attributes: .concurrent)
     let dispatchGroup = DispatchGroup()
-
+    
     // Test adding objects on multiple threads.
     (0..<Self.total).forEach { i in
       dispatchGroup.enter()
       queue.async {
-        self.threadLock.write {
+        self.threadLock.write(isAsync: false) {
           self.testArray.append(i)
         }
-//        self.threadLock.read {
-//          let expected = i + 1
-//          XCTAssertTrue(
-//            self.testArray.count == expected,
-//            "Incorrect testArray.count. expected = \(expected), actual = \(self.testArray.count)"
-//          )
-//        }
+        self.threadLock.read {
+          let expected = i + 1
+          XCTAssertTrue(
+            self.testArray.count == expected,
+            "Incorrect testArray.count. expected = \(expected), actual = \(self.testArray.count)"
+          )
+        }
         dispatchGroup.leave()
       }
     }
