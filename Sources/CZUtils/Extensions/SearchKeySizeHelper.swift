58efbcf0bb6b1edd07796acf039f449e5f3e5132
cheng zhang <showt2me@gmail.com>
2022-06-28 22:54:03 -0700

    built

--- Sources/CZUtils/Extensions/SearchKeySizeHelper.swift
+++ Sources/CZUtils/Extensions/SearchKeySizeHelper.swift
@@ -3,33 +3,6 @@ import UIKit
 
 @objc
 public class SearchKeySizeHelper: NSObject {
-  @objc(sharedInstance)
-  public static let shared = SearchKeySizeHelper()
-
-  private static let deviceCodeSets: [Set<String>] = [
-    // @"iPhone 11 Pro", @"iPhone 12 mini", @"iPhone 13 mini"
-    Set(["iPhone12,3", "iPhone13,1", "iPhone14,4"]),
-    // @"iPhone 12", @"iPhone 12 Pro", @"iPhone 13", @"iPhone 13 Pro"
-    Set(["iPhone13,2", "iPhone13,3", "iPhone14,5", "iPhone14,2"]),
-    // @"iPhone 11", @"iPhone 11 Pro Max"
-    Set(["iPhone12,1", "iPhone12,5"]),
-    // @"iPhone 12 Pro Max", @"iPhone 13 Pro Max"
-    Set(["iPhone13,4", "iPhone14,3"]),
-    // @"iPhone 6s Plus", @"iPhone 8 Plus"
-    Set(["iPhone8,2", "iPhone10,2", "iPhone10,5"]),
-  ]
-
-  private static let searchKeySizes = [
-    CGSize(width: 88, height: 42),
-    CGSize(width: 91, height: 38),
-    CGSize(width: 96, height: 45),
-    CGSize(width: 100, height: 45),
-    CGSize(width: 97, height: 45)
-  ]
-
-  /// The device code of the current device.
-  private static let deviceCode = getDeviceCode()
-
   /// Returns the search key size for the current device state.
   /// - Note If the current device isn't supported by this method, returns CGSizeZero.
   @objc
@@ -46,7 +19,34 @@ public class SearchKeySizeHelper: NSObject {
   }
 }
 
+// MARK: - Private variables / methods
+
 private extension SearchKeySizeHelper {
+
+  static let deviceCodeSets: [Set<String>] = [
+    // @"iPhone 11 Pro", @"iPhone 12 mini", @"iPhone 13 mini"
+    Set(["iPhone12,3", "iPhone13,1", "iPhone14,4"]),
+    // @"iPhone 12", @"iPhone 12 Pro", @"iPhone 13", @"iPhone 13 Pro"
+    Set(["iPhone13,2", "iPhone13,3", "iPhone14,5", "iPhone14,2"]),
+    // @"iPhone 11", @"iPhone 11 Pro Max"
+    Set(["iPhone12,1", "iPhone12,5"]),
+    // @"iPhone 12 Pro Max", @"iPhone 13 Pro Max"
+    Set(["iPhone13,4", "iPhone14,3"]),
+    // @"iPhone 6s Plus", @"iPhone 8 Plus"
+    Set(["iPhone8,2", "iPhone10,2", "iPhone10,5"]),
+  ]
+
+  static let searchKeySizes = [
+    CGSize(width: 88, height: 42),
+    CGSize(width: 91, height: 38),
+    CGSize(width: 96, height: 45),
+    CGSize(width: 100, height: 45),
+    CGSize(width: 97, height: 45)
+  ]
+
+  /// The device code of the current device.
+  static let deviceCode = getDeviceCode()
+
   /// Returns the device code of the current device. e.g. "iPhone12,3".
   static func getDeviceCode() -> String {
     var systemInfo = utsname()
