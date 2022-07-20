04f1243a2ce3773d510ca6bcb0d6f9e9a131219f
Cheng Zhang <showt2me@gmail.com>
2022-07-20 01:22:55 -0700

    Minor update.

--- Sources/CZUtils/Publisher/CZBasePublisher/CZBasePublisher.swift
+++ Sources/CZUtils/Publisher/CZBasePublisher/CZBasePublisher.swift
@@ -1,21 +1,30 @@
 import Foundation
 
-public class CZBasePublisher<SubscriberType> {
-  private var subscribers = ThreadSafeWeakArray<SubscriberType>(allowDuplicates: false)
+/**
+ Generic base bublisher that can be subclassed to customize how to notify subscribers.
+ You should implement notifySubscribers() in your subclass.
+ */
+open class CZBasePublisher<SubscriberType> {
+  public private(set) var subscribers = ThreadSafeWeakArray<SubscriberType>(allowDuplicates: false)
 
   public init() {}
 
   // MARK: - Subscriber
-
-  public func addSubscriber(_ subscriber: SubscriberType) {
+  
+  /// Notify all subscribers that the observed event occurs.
+  open func notifySubscribers() {
+    assertionFailure("You should implement notifySubscribers() in your subclass.")
+  }
+  
+  open func addSubscriber(_ subscriber: SubscriberType) {
     subscribers.append(subscriber)
   }
 
-  public func removeSubscriber(_ subscriber: SubscriberType) {
+  open func removeSubscriber(_ subscriber: SubscriberType) {
     subscribers.remove(subscriber)
   }
 
-  public func removeAllSubscribers() {
+  open func removeAllSubscribers() {
     subscribers.removeAll()
   }
 }
