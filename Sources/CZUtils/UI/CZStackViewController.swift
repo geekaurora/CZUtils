a36264e114dfed5881522fa5285be3681db3ef31
Cheng Zhang <showt2me@gmail.com>
2022-07-20 11:18:36 -0700

    Add CZStackViewController.

---
+++ Sources/CZUtils/UI/CZStackViewController.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+/**
+ Container controller that maintains an underlying StackView for the arranged subviews.
+ 
+ ## Usage
+   1. Subclass CZStackViewController.
+  2. Override `setupViews()` for customization: add arranged subviews etc.
+ */
+open class CZStackViewController: UIViewController {
+  public private(set) var stackView: UIStackView!
+  
+  public override init(nibName nibNameOrNil: String? = nil,
+                       bundle nibBundleOrNil: Bundle? = nil) {
+    super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+  }
+  
+  required public init?(coder: NSCoder) {
+    super.init(coder: coder)
+  }
+  
+  open override func loadView() {
+    self.view = UIView()
+  }
+  
+  open override func viewDidLoad() {
+    super.viewDidLoad()
+    setupViews()
+  }
+
+  /// Set up subviews - you may override it for customization.
+  open func setupViews() {
+    // Set up stackView.
+    stackView = UIStackView()
+    stackView.axis = .vertical
+    view.addSubview(stackView)
+    NSLayoutConstraint.activate(stackView.align(to: view.safeAreaLayoutGuide))
+  }  
+}
