41a37e8cb1632c7ecfde2cf1c37d9b95aa769b64
Tony Zhang <756876+geekaurora@users.noreply.github.com>
2024-04-14 11:50:48 -0700

    Add UIView+AutoLayoutDebug. (#48)
    * Add UIView+AutoLayoutDebug.
    
    * Update code style.
    
    * Minor update.
    
    * minor
    
    * minor

---
+++ Sources/CZUtils/Extensions/UIView+AutoLayoutDebug.swift
@@ -0,0 +1,159 @@
+import UIKit
+
+#if DEBUG
+
+/// Helper methods for debugging Auto Layout.
+@objc
+extension UIView {
+  /// Prints the layout reports for the view and its subviews.
+  @objc
+  public func cz_printLayoutReports() {
+    print("\n=============================\n\(#function)\n=============================")
+    cz_printLayoutReports(allowRecursion: true)
+  }
+
+  /// Prints the layout reports for the view and its subviews with `allowRecursion`.
+  /// - Note: The method doesn't support ObjC.
+  public func cz_printLayoutReports(allowRecursion: Bool) {
+    print(cz_layoutDescription)
+
+    guard allowRecursion else { return }
+
+    for clazz in UIView.skippableClasses where self.isKind(of: clazz) {
+      return
+    }
+
+    for view in subviews {
+      view.cz_printLayoutReports(allowRecursion: allowRecursion)
+    }
+  }
+
+  /// Returns the layout report for the view.
+  @objc
+  public var cz_layoutDescription: String {
+    var description = "<\(Unmanaged.passUnretained(self).toOpaque())> \(type(of: self)) : \(type(of: self).superclass()!)"
+
+    if translatesAutoresizingMaskIntoConstraints {
+      description += " [Autosizes]"
+    }
+
+    // Check whether the view has ambiguous layouts.
+    // Note: If `hasAmbiguousLayout` is true, UIKit will display an warning of
+    // "View has an ambiguous layout" automatically.
+    if hasAmbiguousLayout {
+      description += "\n\n----\n[Caution!] FOUND Ambiguous Layouts!"
+    }
+
+    description += "\nContent size...\(sizeString(intrinsicContentSize))"
+
+    if intrinsicContentSize.width > 0 || intrinsicContentSize.height > 0 {
+      description += " [Content Mode: \(UIView.nameForContentMode(contentMode))]"
+    }
+
+    description += "\nHugging........[H \(hugValueH)] [V \(hugValueV)]\n"
+    description += "Resistance.....[H \(resistValueH)] [V \(resistValueV))]\n"
+    description += "Constraints....\(constraints.count)\n"
+
+    for i in 0..<constraints.count {
+      let constraint = constraints[i]
+      description += String(format: "%2d. ", i)
+      description += "@\(constraint.priority) \(constraint) \n"
+    }
+
+    return description
+  }
+
+  /// Returns all constraints that refer to the view.
+  @objc
+  public var cz_referencingConstraints: Set<NSLayoutConstraint> {
+    let referencingConstraints = Set(constraints.filter { $0.refersToView(self) })
+    return referencingConstraints.union(referencingConstraintsInSuperviews)
+  }
+
+  // MARK: - Private methods
+
+  fileprivate var referencingConstraintsInSuperviews: Set<NSLayoutConstraint> {
+    var results = Set<NSLayoutConstraint>()
+
+    var currentView: UIView? = self
+    while let view = currentView?.superview {
+      let referencingConstraints = Set(view.constraints.filter { $0.refersToView(self) })
+      results = results.union(referencingConstraints)
+
+      currentView = view
+    }
+    return results
+  }
+
+
+  fileprivate var hugValueH: Float {
+    return Float(contentHuggingPriority(for: .horizontal).rawValue)
+  }
+
+  fileprivate var hugValueV: Float {
+    return Float(contentHuggingPriority(for: .vertical).rawValue)
+  }
+
+  fileprivate var resistValueH: Float {
+    return Float(contentCompressionResistancePriority(for: .horizontal).rawValue)
+  }
+
+  fileprivate var resistValueV: Float {
+    return Float(contentCompressionResistancePriority(for: .vertical).rawValue)
+  }
+
+  fileprivate static func nameForContentMode(_ contentMode: UIView.ContentMode) -> String {
+    switch contentMode {
+    case .scaleToFill:
+      return "ScaleToFill"
+    case .scaleAspectFit:
+      return "ScaleAspectFit"
+    case .scaleAspectFill:
+      return "ScaleAspectFill"
+    case .redraw:
+      return "Redraw"
+    case .center:
+      return "Center"
+    case .top:
+      return "Top"
+    case .bottom:
+      return "Bottom"
+    case .left:
+      return "Left"
+    case .right:
+      return "Right"
+    case .topLeft:
+      return "TopLeft"
+    case .topRight:
+      return "TopRight"
+    case .bottomLeft:
+      return "BottomLeft"
+    case .bottomRight:
+      return "BottomRight"
+    default:
+      return "Unknown"
+    }
+  }
+
+  fileprivate static var skippableClasses: [UIView.Type] {
+    return [UIButton.self, UILabel.self, UISwitch.self, UIStepper.self, UITextField.self, UIScrollView.self, UIActivityIndicatorView.self, UIAlertView.self, UIPickerView.self, UIProgressView.self, UIToolbar.self, UINavigationBar.self, UISearchBar.self, UITabBar.self]
+  }
+}
+
+fileprivate extension NSLayoutConstraint {
+  func refersToView(_ view: UIView) -> Bool {
+    return firstItem === view || secondItem === view
+  }
+}
+
+fileprivate extension CGFloat {
+  var truncatingDecimals: String {
+    return String(format: "%.0f", self)
+  }
+}
+
+fileprivate func sizeString(_ size: CGSize) -> String {
+  return "(\(size.width.truncatingDecimals), \(size.height.truncatingDecimals))"
+}
+
+#endif
