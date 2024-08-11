d7449212fed9f335c1e1aeb116177cceda75bf92
cheng zhang <showt2me@gmail.com>
2024-08-10 18:08:59 -0700

    Error+: add cz_description variable.

--- Sources/CZUtils/Extensions/Error+.swift
+++ Sources/CZUtils/Extensions/Error+.swift
@@ -8,8 +8,36 @@
 
 import Foundation
 
+// MARK: - Error
+
 public extension Error {
-    var retrievedCode: Int {
-        return (self as NSError).code
+  var retrievedCode: Int {
+    return (self as NSError).code
+  }
+
+  /// Detailed description including the `userInfo` variable.
+  var cz_description: String {
+    (self as NSError).cz_description
+  }
+}
+
+// MARK: - NSError
+
+public extension NSError {
+  /// Detailed description including the `userInfo` variable.
+  var cz_description: String {
+    var description = "\(self)"
+
+    var userInfoDataString = ""
+    if let userInfoData = userInfo["data"] as? Data {
+      if let userInfoDataDict: [String: Any]? = CZHTTPJsonSerializer.deserializedObject(with: userInfoData) {
+        userInfoDataString = userInfoDataDict?.description ?? ""
+      } else {
+        userInfoDataString = String(data: userInfoData, encoding: .utf8) ?? ""
+      }
+      description += "\n\n" + "userInfo['data'] = \n\(userInfoDataString))"
     }
+
+    return description
+  }
 }
