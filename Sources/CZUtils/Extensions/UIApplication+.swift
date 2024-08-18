e92f220157b34316c3593df1a2d96c5aa94ac10b
cheng zhang <showt2me@gmail.com>
2024-08-18 13:09:39 -0700

    topMostViewController(on:) - change the checking order.

--- Sources/CZUtils/Extensions/UIApplication+.swift
+++ Sources/CZUtils/Extensions/UIApplication+.swift
@@ -8,23 +8,24 @@ public extension UIApplication {
   /// - Returns: top most view controller on `inputController`.
   func topMostViewController(on inputController: UIViewController? = nil) -> UIViewController? {
     let controller = inputController ?? self.keyWindow?.rootViewController
-    
-    if let topChildController = controller?.children.last {
-      return topMostViewController(on: topChildController)
-    }
-    if let navigationController = controller as? UINavigationController {
-      return topMostViewController(on: navigationController.visibleViewController)
-    }
+
     if let tabController = controller as? UITabBarController {
       if let selected = tabController.selectedViewController {
         return topMostViewController(on: selected)
       }
     }
+
+    if let navigationController = controller as? UINavigationController {
+      return topMostViewController(on: navigationController.visibleViewController)
+    }
+
+    if let topChildController = controller?.children.last {
+      return topMostViewController(on: topChildController)
+    }
+
     if let presented = controller?.presentedViewController {
       return topMostViewController(on: presented)
     }
     return controller
   }
 }
-
-
