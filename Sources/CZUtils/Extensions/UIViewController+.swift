03e2add977fa9303c50b4946dee6ad4ea6ad027f
cheng zhang <showt2me@gmail.com>
2024-09-18 17:48:34 -0700

    Update UIViewController+

--- Sources/CZUtils/Extensions/UIViewController+.swift
+++ Sources/CZUtils/Extensions/UIViewController+.swift
@@ -7,24 +7,11 @@
 
 import UIKit
 
-@objc public extension UIViewController {
-  /// Stick the intput view's edges to topLayoutGuide, bottomLayoutGuide, leading, trailing.
-  func overlaySubViewOnSelf(_ subview: UIView) {
-    subview.translatesAutoresizingMaskIntoConstraints = false
-    NSLayoutConstraint.activate([
-      subview.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-      subview.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-      subview.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-      subview.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-    ])
-  }
-  
-  /// Overlay self on super ViewController.
-  func overlayOnSuperViewController(_ controller: UIViewController, insets: UIEdgeInsets = .zero) {
-    view.overlayOnSuperViewController(controller)
-    controller.addChildAndSetDidMoveToParent(self)
-  }
-  
+@objc 
+public extension UIViewController {
+
+  // MARK: - Order
+
   /// Returns top most ViewController on the current keyWindow.
   class func topMost(on controller: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
     if let navigationController = controller as? UINavigationController {
@@ -39,6 +26,48 @@ import UIKit
       return topMost(on: presented)
     }
     return controller
+  }
+
+  // MARK: - Title
+
+  func showTitleOnNavBar() {
+    let internalTitleView: UIView
+    if let image = UIImage(named: "InstagramTitle") {
+      internalTitleView = UIImageView(image: image)
+    } else {
+      let titleView = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 30))
+      titleView.textAlignment = .center
+      titleView.text = "Instagram"
+      titleView.font = UIFont(name: "Baskerville-SemiBoldItalic", size: 25)
+      titleView.textColor = UIColor(white: 0.22, alpha: 1)
+      internalTitleView = titleView
+    }
+    navigationItem.titleView = internalTitleView
+  }
+
+  func setTabTitle(_ title: String, at index: Int) {
+    guard let tabBarItems = tabBarController?.tabBar.items,
+      0..<tabBarItems.count ~= index else { return }
+    tabBarItems[index].title = NSLocalizedString(title, comment: "")
+  }
+
+  // MARK: - Parent / Child
+
+  /// Stick the intput view's edges to topLayoutGuide, bottomLayoutGuide, leading, trailing.
+  func overlaySubViewOnSelf(_ subview: UIView) {
+    subview.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      subview.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+      subview.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      subview.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+      subview.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+    ])
+  }
+
+  /// Overlay self on super ViewController.
+  func overlayOnSuperViewController(_ controller: UIViewController, insets: UIEdgeInsets = .zero) {
+    view.overlayOnSuperViewController(controller)
+    controller.addChildAndSetDidMoveToParent(self)
   }
   
   /// Adds child ViewController and calls didMove(toParent: self) automatically.
@@ -62,26 +91,16 @@ import UIKit
     }
     removeFromParent()
   }
-  
-  func showTitleOnNavBar() {
-    let internalTitleView: UIView
-    if let image = UIImage(named: "InstagramTitle") {
-      internalTitleView = UIImageView(image: image)
+
+  // MARK: - Presentation
+
+  /// Pops the controller if it's in a navigationController, otherwise dismisses the controller modally.
+  func dismissIfNeeded(animated: Bool) {
+    if let navigationController = self.navigationController {
+      navigationController.popViewController(animated: animated)
     } else {
-      let titleView = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 30))
-      titleView.textAlignment = .center
-      titleView.text = "Instagram"
-      titleView.font = UIFont(name: "Baskerville-SemiBoldItalic", size: 25)
-      titleView.textColor = UIColor(white: 0.22, alpha: 1)
-      internalTitleView = titleView
+      dismiss(animated: animated)
     }
-    navigationItem.titleView = internalTitleView
-  }
-  
-  func setTabTitle(_ title: String, at index: Int) {
-    guard let tabBarItems = tabBarController?.tabBar.items,
-      0..<tabBarItems.count ~= index else { return }
-    tabBarItems[index].title = NSLocalizedString(title, comment: "")
   }
 }
 
